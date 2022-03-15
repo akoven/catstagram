800f15378e6e5ae51fdeb7181cf8130d3d7c0116
Attiya <adarensburg@live.com>
2022-03-15 17:00:12 -0500

    break

--- public/index.js
+++ public/index.js
@@ -1,1 +1,12 @@
-// Your code here
+window.onload = () =>{
+    const header = document.createElement("h1")
+    header.innerText = "kitten pic";
+    const image = document.createElement("img");
+    // let url = 'https://cdn2.thecatapi.com/images/ccc.png'
+    let url = fetch("https://api.thecatapi.com/v1/images/0SxW2SQ_S")
+    // .then(res => { res.json() });
+    // .then(body => {body.url});
+    image.src = `${url}`
+    document.body.append(header,image)
+};
+// window.onload();
